8df56f616c596bb5ca47cc775b577f6030ec51e3
malcovys <malcovys22.aps@gmail.com>
2024-07-10 13:28:54 +0300

    init

---
+++ messenger_client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  let helloWorld = "Hello world";
+
+  return (
+    <>
+      <div className='flex justify-center items-center'>
+        <h1>{helloWorld}</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
