5b918a2d63bb01138c99d78a99fc0cacbd0c4d8a
malcovys <malcovys22.aps@gmail.com>
2024-07-17 19:05:42 +0300

    mpd

---
+++ messenger_api/database/migrations/2024_07_17_153625_create_calls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('ended')->default(false);
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(\App\Models\Message::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calls');
+    }
+};
